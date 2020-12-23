efb9341368b2797b37d72cf525c6aeee6d10b476
NaudEric <eric.naud@etu.unice.fr>
2020-12-24 00:25:46 +0100

    #36

--- src/main/java/fr/unice/polytech/startingpoint/bot/Bot.java
+++ src/main/java/fr/unice/polytech/startingpoint/bot/Bot.java
@@ -32,7 +32,7 @@ import java.util.List;
 
 public abstract class Bot {
     protected final PlayerInteraction playerInteraction;
-
+    int NB_ACTION = 2;
     /**
      * <p>Set up the bot. Initialize all variables.</p>
      *
